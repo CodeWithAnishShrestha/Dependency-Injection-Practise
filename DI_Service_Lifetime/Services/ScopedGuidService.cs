4187f2b2e494bfc913fb6cdae221ce039802db74
CodeWithAnishShrestha <growwithanish@gmail.com>
2024-04-23 14:13:14 +0545

    Add project files.

---
+++ DI_Service_Lifetime/Services/ScopedGuidService.cs
@@ -0,0 +1,10 @@
+﻿namespace DI_Service_Lifetime.Services
+{
+    public class ScopedGuidService : IScopedGuidService
+    {
+        public string GetGuid()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
